8c62ccf4595d3e2dccc865c28cfff69fa55047a7
jdowns525 <downsjosseph@gmail.com>
2023-02-20 20:57:22 +0000

    hi

---
+++ my_qr.rb
@@ -0,0 +1,22 @@
+their_answer = "Yes"
+
+desired_url = "https://jdowns525.github.io/"
+
+
+puts "Would you like to make a QR Code?"
+their_answer = gets
+puts "Okay, enter desired URL to generate QR Code."
+their_answer = gets
+
+
+
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new(their_answer)
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
